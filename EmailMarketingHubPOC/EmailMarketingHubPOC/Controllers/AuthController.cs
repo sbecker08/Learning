14b42436e6ee10cc45c5922b59787fd576edc833
sbecker08 <sbecker008@gmail.com>
2016-02-18 17:48:29 -0800

    Added Email Marketing Hub

---
+++ EmailMarketingHubPOC/EmailMarketingHubPOC/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmailMarketingHubPOC.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Index()
+        {
+            //if query string param "q" exists it is a post back
+            if (Request.QueryString["q"] != null)
+            {
+                MNet_Common.Auth.ProcessAuthResponse(ConfigurationManager.AppSettings["EncryptionKey"], Request.QueryString["q"]);
+            }
+            else
+            {
+                MNet_Common.Auth.SendAuthRequest(ConfigurationManager.AppSettings["EncryptionKey"]);
+            }
+            //it shouldn't return anything as all the auth controller should do is handle the authentication redirects (either to MNet or back to the originating page)
+            return View();
+        }
+    }
+}
---
+++ EmailMarketingHubPOC/EmailMarketingHubPOC/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmailMarketingHubPOC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            //var user = System.Web.HttpContext.Current.User.Identity.Name.Replace("mmreibc\\", "");
+            return View();
+        }
+
+
+
+        public ActionResult Authorize()
+        {
+            var APIKey = "mdyvurwe4m3etfvkga8nzrp6";
+            var secret = "C3TqYbYxYzb3RC9udfcRW8AV";
+            var redirect = "http://emailmarketinghub.mmreibc.prv/Home/AuthorizeResponse";
+            var url = "https://oauth2.constantcontact.com/oauth2/oauth/siteowner/authorize?response_type=code&client_id=" + APIKey + "&redirect_uri=" + redirect;
+            return Redirect(url);
+        }
+        //
+        public ActionResult AuthorizeResponse(string code, string username)
+        {
+            ViewBag.Message = "Your contact page.";
+
+            var APIKey = "mdyvurwe4m3etfvkga8nzrp6";
+            var secret = "C3TqYbYxYzb3RC9udfcRW8AV";
+            var redirect = "http://emailmarketinghub.mmreibc.prv/Home/AuthorizeResponse";
+
+            //POST
+            var url = "https://oauth2.constantcontact.com/oauth2/oauth/token?grant_type=authorization_code&client_id=" + APIKey + "&client_secret=" + secret + "&code=" + code + "&redirect_uri=" + redirect;
+
+            string token = "";
+            using (WebClient wc = new WebClient())
+            {
+                token = wc.UploadString(url, "");
+            }
+
+            //var token = "{\"access_token\": \"c9be2bad-ba98-4129-a942-215011e4e215\",\"expires_in\": 315359873,\"token_type\": \"Bearer\"}";
+
+            //var token = "{\"access_token\": \"c9be2bad-ba98-4129-a942-215011e4e215\",\"expires_in\": 315359873,\"token_type\": \"Bearer\"}";
+            var auth = token.Split(',')[0].Substring(18, 36);
+
+            return View();
+        }
+    }
+}
